d175f953f7231062cf8c697f44df922c6b15f827
repu1sion <bearrailgun@gmail.com>
2018-08-14 21:04:21 +0100

    build script improved

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+JOBS=`nproc`
+
+if [ $# -eq 1 ]; then
+	echo "setting jobs to cmdline param"
+	JOBS=$1
+fi
+
+echo "jobs number is: $JOBS"
+
 ./bootstrap
 ./configure --enable-dpdk --disable-static-applications --with-dpdk-path=/root/dpdk/dpdk/x86_64-native-linuxapp-gcc/
-make && echo "success" || echo "fail"
+make -j${JOBS} && echo "success" || echo "fail"
 make install
 ldconfig
